008e76e0ba1ba69609bf9738b9ea46098f6b1115
Matias Francois <mfrancois@itba.edu.ar>
2021-02-05 02:45:48 -0300

    uploading project

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    //horizontal reffers to the predefined variables that you can see in project settings->input
+    public string inputSteerAxis = "Horizontal";
+    public string inputThroattleAxis = "Vertical";
+
+    public float ThroattelInput { get; private set; }
+    public float SteerInput { get; private set; }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SteerInput = Input.GetAxis(inputSteerAxis);
+        ThroattelInput = Input.GetAxis(inputThroattleAxis);
+    }
+}
---
+++ Assets/Scripts/car.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    public Transform centerOfMass;
+    public float motorTorque = 1500f;
+    public float maxSteer = 20f;
+
+    public float Steer { get; set; }
+    public float Throttle { get; set; }
+
+    private Rigidbody _rigibody;
+    private Wheel[] wheels;
+
+    private void Start()
+    {
+        //get the wheel components of it children
+        wheels = GetComponentsInChildren<Wheel>();
+        _rigibody = GetComponent<Rigidbody>();
+        _rigibody.centerOfMass = centerOfMass.localPosition;
+    }
+
+    void Update()
+    {
+        Steer = GameManager.Instance.InputController.SteerInput;
+        Throttle = GameManager.Instance.InputController.ThroattelInput;
+
+        foreach (var wheel in wheels)
+        {
+            wheel.SteerAngle = Steer * maxSteer;
+            wheel.Torque = Throttle * motorTorque;
+        }
+    }
+
+}
+
+
