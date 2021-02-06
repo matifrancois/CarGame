bafb3963dbed0133c9638937fcaf1ff27002b329
Matias Francois <mfrancois@itba.edu.ar>
2021-02-06 20:56:48 -0300

    times and info working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     public float CurrentLapTime { get; private set; } = 0.0F;
     public int currentLap { get; private set; } = 0;
 
-    private float lapTimer;
+    private float lapTimerTimerStamp = 0.0F;
     private int lastCheckpointPassed = 0;
 
     private Transform checkPointsParent;
@@ -24,16 +24,64 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         checkPointsParent = GameObject.Find("CheckPoints").transform;
-        checkPointCount = checkPointsParent.childCount;
+        checkPointCount = checkPointsParent.childCount-1;
         //converts the strings checkpoints to number
         checkPointLayer = LayerMask.NameToLayer("CheckPoints");
         carController = GetComponent<CarController>();
     }
 
+    void StartLap()
+    {
+        Debug.Log("Start Lap");
+        currentLap++;
+        lapTimerTimerStamp = Time.time;
+    }
+
+    void EndLap()
+    {
+        LastLapTime = Time.time - lapTimerTimerStamp;
+        lapTimerTimerStamp = Time.time;
+        BestLapTime = Mathf.Min(LastLapTime, BestLapTime);
+        currentLap++;
+        lastCheckpointPassed = 0;
+        //Debug.Log("End Lap: Lap Time was: " + LastLapTime + "seconds");
+    }
+
+    //enter here if the object collides with another object
+    private void OnTriggerEnter(Collider collider)
+    {
+        if ( collider.gameObject.layer != checkPointLayer)
+        {
+            return;
+        }
+        //if you pass the finish line
+        if (collider.gameObject.name == "start")
+        {
+            //and if the lastCheckpoint was the last of the checkpoints in the lap
+            if (lastCheckpointPassed == checkPointCount)
+            {
+                EndLap();
+            }
+            //if you are here it means that passed the start line so we call startLap method
+            if (currentLap == 0)
+            {
+                StartLap();
+                Debug.Log("started");
+            }
+            return;
+        }
+        if (collider.gameObject.name == (lastCheckpointPassed + 1).ToString())
+        {
+            lastCheckpointPassed++;
+            Debug.Log(lastCheckpointPassed);
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
+        CurrentLapTime = lapTimerTimerStamp > 0.0F ? Time.time - lapTimerTimerStamp : 0.0F;
+
         if (controlType == ControlType.HumanInput)
         {
             carController.Steer = GameManager.Instance.InputController.SteerInput;
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject UIRacePanel;
+
+    public Text UITextCurrentLap;
+    public Text UITextCurrentTime;
+    public Text UITextLastLapTime;
+    public Text UITextBestLapTime;
+
+    public Player UpdateUIForPlayer;
+
+    private int currentLap = -1;
+    private float currentTime = -1.0F;
+    private float lastLapTime;
+    private float bestLapTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(UpdateUIForPlayer == null)
+            return;
+        if (UpdateUIForPlayer.currentLap != currentLap)
+        {
+            currentLap = UpdateUIForPlayer.currentLap;
+            UITextCurrentLap.text = $"Lap: {currentLap}";
+        }
+
+        if (UpdateUIForPlayer.CurrentLapTime != currentTime)
+        {
+            currentTime = UpdateUIForPlayer.CurrentLapTime;
+            UITextCurrentTime.text = $"Time: {(int)currentTime / 60}:{(currentTime) % 60:00.000}";
+        }
+
+        if(UpdateUIForPlayer.LastLapTime != lastLapTime)
+        {
+            lastLapTime = UpdateUIForPlayer.LastLapTime;
+            UITextLastLapTime.text = $"Last Lap: {(int)lastLapTime / 60}:{(lastLapTime) % 60:00.000}";
+        }
+
+        if (UpdateUIForPlayer.BestLapTime != bestLapTime)
+        {
+            bestLapTime = UpdateUIForPlayer.BestLapTime;
+            UITextBestLapTime.text = UpdateUIForPlayer.BestLapTime == Mathf.Infinity ? $"Best Lap: {(int)0.0F / 60}:{(0.0F) % 60:00.000}" : $"Best Lap: {(int)bestLapTime / 60}:{(bestLapTime) % 60:00.000}";
+        }
+    }
+}
